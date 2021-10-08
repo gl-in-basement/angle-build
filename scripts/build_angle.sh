be04a14c6b7fdcdcb0586490780bab384f0071c3
gjz010 <gjz010944@gmail.com>
2021-10-09 07:16:59 +0800

    angle upload

---
+++ scripts/build_angle.sh
@@ -0,0 +1,10 @@
+export PATH=$PWD/depot_tools:$PATH
+echo "Building angle"
+cd angle
+python scripts/bootstrap.py
+gclient sync
+git checkout main
+cat ../patches/angle-enable-static.txt
+gen out/Release --args='is_debug=false is_clang=true is_componented_build=false angle_assert_always_on=true'
+autoninja -C out/Release
+tar -czvf ../angle.tar.gz out/Release
