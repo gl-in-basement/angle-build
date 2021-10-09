d74906fe35d5c82171178726dd5d006be42ebbba
gjz010 <gjz010944@gmail.com>
2021-10-09 09:52:56 +0800

    upload entire directory without creating a tarball

--- scripts/build_angle.sh
+++ scripts/build_angle.sh
@@ -5,6 +5,5 @@ python scripts/bootstrap.py
 gclient sync
 git checkout main
 cat ../patches/angle-enable-static.txt >> BUILD.gn
-gn gen out/Release --args='is_debug=false is_clang=true is_componented_build=false angle_assert_always_on=true'
+gn gen out/Release --args='is_debug=false is_clang=true is_component_build=false angle_assert_always_on=true'
 autoninja -C out/Release
-tar -czvf ../angle.tar.gz out/Release
