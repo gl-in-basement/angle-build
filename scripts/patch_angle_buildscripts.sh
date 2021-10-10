26a1a3867539650c1ced02ee879a2124ebb8cfb0
gjz010 <gjz010944@gmail.com>
2021-10-10 11:09:07 +0800

    p1

--- scripts/patch_angle_buildscripts.sh
+++ scripts/patch_angle_buildscripts.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 shopt -s inherit_errexit
 echo "Patching buildscripts"
 cd angle
-patch -p0 -i ../patches/Build-Static-Library.patch
-patch -p0 -i ../patches/Disable-Thin-Library.patch
+patch -p1 -i ../patches/Build-Static-Library.patch
+patch -p1 -i ../patches/Disable-Thin-Library.patch
